d903697581b24f03488a3c5d6a2adf8e3308a05d
PeterBookmyer <101828030+PeterBookmyer@users.noreply.github.com>
2022-06-01 11:16:05 -0600

    updating changes to class activities

--- 10-OOP/28-Stu_Mini-Project/Develop/lib/Letter.js
+++ 10-OOP/28-Stu_Mini-Project/Develop/lib/Letter.js
@@ -1,5 +1,13 @@
 class Letter {
-  
+  constructor(letter) {
+    this.char = letter;
+    this.visible = "!@#$%^&*()".includes(letter) ? true : false;
+    this.lastGuess = null;
+  }
+  guess(userInput) {
+    this.lastGuess = userInput;
+    if (this.lastGuess === this.letter) return;
+  }
 }
 
 module.exports = Letter;
--- 10-OOP/28-Stu_Mini-Project/Develop/lib/Word.js
+++ 10-OOP/28-Stu_Mini-Project/Develop/lib/Word.js
@@ -1,7 +1,10 @@
 const Letter = require("./Letter");
 
 class Word {
-  
+  constructor(word, letters){
+      this.word = word;
+      this.letters = word.split('')
+  }
 }
 
 module.exports = Word;
